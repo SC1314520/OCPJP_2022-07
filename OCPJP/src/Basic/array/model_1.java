12e2aebd0ce79a1af92668db9035ed4f9af55a06
SC1314520 <blc0000421@gmail.com>
2022-07-28 16:40:18 +0800

    practice

--- OCPJP/src/Basic/array/model_1.java
+++ OCPJP/src/Basic/array/model_1.java
@@ -1,5 +1,7 @@
 package Basic.array;
 
+import java.util.Arrays;
+
 public class model_1 {
 	public static void main(String[] args) {
 		// not array
@@ -10,14 +12,26 @@ public class model_1 {
 		System.out.println(abc);
 		System.out.println(def);
 		System.out.println(ghi);
-		//-------------------------------//
+		
+		//--------------陣列宣告法-----------------//  
 		// array-1
-		int[] array=new int[3];
+		int[] array=new int[3]; // new 給 default 初始值 --> 動態初始值
 		array[0]=123;
 		array[1]=456;
 		array[2]=789;
+		
 		//array-2
 		int[] arrays= {123,456,789};
+		
+		//array-3
+		int[] arrayss=new int[] {}; //陣列長度為 0
+		System.out.println(arrayss.length); //陣列長度為 0
+		//System.out.println(arrayss[0]); -->java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
+		int[] arraysss= {}; 
+		System.out.println(arraysss.length); //陣列長度為 0
+		//System.out.println(arraysss[0]); -->java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
+		
+		//----------------- for 迴圈運用-----------------------//
 		
 		// for loop
 		for(int i=0;i<array.length;i++) {
@@ -32,6 +46,40 @@ public class model_1 {
 		}
 		for(int k : arrays) {
 			System.out.println(k);
+		}
+		
+		
+		//-----------------型態比大小-----------------------//
+		int[] d =new int[3];
+		for(int z:d) { // z的型態必須大於等於陣列型態。
+			System.out.println(z); //0      
+		}
+		for(double z:d) { // double>int 所以可以這樣寫。
+			System.out.println(z); // 0.0
+		}
+		
+		
+		//------------------包覆類別與基本型態互換 I----------------------//
+		Integer[] i2= new Integer[3];
+		for(Integer i :i2) {
+			System.out.println(i); //null
+		}
+		//----------------------------------------//
+		Integer[] i3= {1,2,3};
+		for(int i:i3) { // Integer 可用 int 接收
+			System.out.println(i); // 輸出: 1,2,3
+		}
+		//----------------------------------------//
+		int[] i4 = {1,2,3};
+		for(Integer i:i4) { // int 可用 Integer 接收
+			System.out.println(i); //輸出: 1,2,3
+		
+		}
+		
+		//------------------包覆類別與基本型態互換 II ----------------------//
+		Integer[] i5= new Integer[3]; // 基本型別不可接收 null !!!
+		for(int i :i5) {
+			//System.out.println(i); --> java.lang.NullPointerException
 		}
 	}
 }
---
+++ OCPJP/src/Basic/array/model_2.java
@@ -0,0 +1,30 @@
+package Basic.array;
+
+import java.util.Arrays;
+
+public class model_2 {
+	public static void main(String[] args) {
+		
+		// 基本型別
+		int[] array=new int[3];
+		double[] arrays =new double[3];
+		boolean[] arrays2 = new boolean[3];
+		// 包覆類別: class 的預設值皆是 null
+		String[] arrays3 =new String[3];
+		Integer[] arrays4 =new Integer[3];
+		Boolean[] arrays5 =new Boolean[3];
+		
+		System.out.println(array[0]); // 0
+		System.out.println(arrays[0]); // 0.0
+		System.out.println(arrays2[0]); //false
+		System.out.println(arrays3[0]); //null
+		System.out.println(arrays4[0]); //null
+		System.out.println(arrays5[0]); //null
+		
+		//--------------------------------------//
+		int[] s= {1,2,3};
+		System.out.println(s); //產生記憶體位置
+		System.out.println(s.toString());//產生記憶體位置
+		System.out.println(Arrays.toString(s)); // 輸出:[1,2,3]
+	}
+}
