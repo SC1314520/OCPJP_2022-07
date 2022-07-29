5af1bc7b563cf054dee38df18d1192fb797d07a5
SC1314520 <blc0000421@gmail.com>
2022-07-29 14:28:26 +0800

    practice

--- OCPJP/src/Basic/array/model_5_value_address.java
+++ OCPJP/src/Basic/array/model_5_value_address.java
@@ -1,5 +1,7 @@
 package Basic.array;
 
+import java.util.Arrays;
+
 public class model_5_value_address {
 	public static void main(String[] args) {
 		int[] x =new int[] {10,20,30};
@@ -8,13 +10,15 @@ public class model_5_value_address {
 		System.out.println(y); // [I@7ad041f3
 		System.out.println(x[0]); //10
 		System.out.println(y[0]); //40
-		x=y; //位址＝位址 pass by address --> 消滅 --> 回收記憶體 --> garbage collection --> gc
+		x=y; //位址＝位址 pass by address --> 消滅(左側被消滅) --> 回收記憶體 --> garbage collection --> gc 
 		// x[0]=y[0] 值＝值 pass by values (copy) 
 		System.out.println(x); // [I@7ad041f3
 		System.out.println(y); // [I@7ad041f3
 		// 兩者已指向同一個記憶體位置（視為一體）。
 		System.out.println(x[0]); // 40
 		System.out.println(y[0]); //40
+		System.out.println(Arrays.toString(x)); //[40, 50, 60]
+		System.out.println(Arrays.toString(y)); //[40, 50, 60]
 		x[0]=100; // 其中一個改變，另一個跟著改變！！
 		System.out.println(x[0]); //100
 		System.out.println(y[0]); //100
@@ -24,7 +28,7 @@ public class model_5_value_address {
 		int j=30;
 		System.out.println(i); //10
 		System.out.println(j); //30
-		i=j; // pass by values
+		i=j; // pass by values 傳值
 		System.out.println(i); //30
 		System.out.println(j); //30
 		j=40; // 其中一個改變，另一個不會跟著改變！！
