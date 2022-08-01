80656f613ed5bc24c3f2f95fb746446259d1bfdd
SC1314520 <blc0000421@gmail.com>
2022-08-01 15:42:42 +0800

    practice

--- OCPJP/src/Basic/array/object_oriented_array/main/demo_2.java
+++ OCPJP/src/Basic/array/object_oriented_array/main/demo_2.java
@@ -41,5 +41,32 @@ public class demo_2 {
 		x[1][0]=new demo_2_method[2];
 		x[1][1]=new demo_2_method[3];
 		x[1][2]=new demo_2_method[4];
+		
+		x[0][0][0]=new demo_2_method("a1","rr");
+		x[0][0][1]=new demo_2_method("a2","rr");
+
+		x[0][1][0]=new demo_2_method("b1","rr");
+		x[0][1][1]=new demo_2_method("b2","rr");
+		x[0][1][2]=new demo_2_method("b3","rr");
+
+		x[1][0][0]=new demo_2_method("c1","rr");
+		x[1][0][1]=new demo_2_method("c2","rr");
+
+		x[1][1][0]=new demo_2_method("d1","rr");
+		x[1][1][1]=new demo_2_method("d2","rr");
+		x[1][1][2]=new demo_2_method("d3","rr");
+
+		x[1][2][0]=new demo_2_method("e1","rr");
+		x[1][2][1]=new demo_2_method("e2","rr");
+		x[1][2][2]=new demo_2_method("e3","rr");
+		x[1][2][3]=new demo_2_method("e4","rr");
+		
+		for(demo_2_method[][] i:x) {
+			for(demo_2_method[] j:i) {
+				for(demo_2_method k:j) {
+					System.out.println(k.show()); 
+				}
+			}
+		}
 	}
 }
