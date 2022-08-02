d79df7620018b759c6f5544cccac5d44b8d1c1c7
SC1314520 <blc0000421@gmail.com>
2022-08-02 16:31:44 +0800

    practice

--- OCPJP/src/Basic/inherit/demo_2_constructor/SB.java
+++ OCPJP/src/Basic/inherit/demo_2_constructor/SB.java
@@ -1,5 +1,9 @@
 package Basic.inherit.demo_2_constructor;
 
 public class SB extends school {
-	
+	int math;
+	public SB(String name,int math) {
+		super(name);
+		this.math=math;
+	}
 }
--- OCPJP/src/Basic/inherit/demo_2_constructor/test.java
+++ OCPJP/src/Basic/inherit/demo_2_constructor/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		SA sa =new SA();   // new school! new SA!   --> 父類別會先執行!!! --> 因為super()必須放在第一行。
 		new SA("Mary",80); // new school Mary    new SA(String name,int chi) 80
+		new SB("Kelly",75); // new school Kelly
 	}
 }
