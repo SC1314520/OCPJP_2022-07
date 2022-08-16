3d6ae50d6eb8134a73116aeee417d0b4706ecb98
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-08-16 16:29:56 +0800

    practice

--- OCPJP/src/Basic/exception/third/ex2.java
+++ OCPJP/src/Basic/exception/third/ex2.java
@@ -25,13 +25,14 @@ public class ex2 {
 			// 若索引碼超出陣列範圍，則會拋出例外，以下程式碼將不會執行。
 			
 			System.out.print("請輸入分母：");
-			double m =sc.nextDouble();
-			System.out.println("x["+n+"]/"+m+" = "+(x[n]/m));
+			int m =sc.nextInt();
+			System.out.println("x["+n+"]/"+m+" = "+((double)x[n]/(double)m));
 			
 			
 			System.out.println("success");
 			
-		} catch (ArrayIndexOutOfBoundsException e) {  // ArrayIndexOutOfBoundsException e =new ArrayIndexOutOfBoundsException();
+			// 注意事項： 只能一槓 且 彼此不能有父子關係 --> Java 7 以後才能這樣寫！！
+		} catch (ArrayIndexOutOfBoundsException  | StringIndexOutOfBoundsException e) {  // ArrayIndexOutOfBoundsException e =new ArrayIndexOutOfBoundsException();
 			System.out.println("index 須介於 0~9 之間");
 			
 			e.printStackTrace(); // java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 10 at Basic.exception.third.ex2.main(ex2.java:25)
@@ -40,9 +41,13 @@ public class ex2 {
 			System.out.println(e.getMessage()); // Index 10 out of bounds for length 10
 			
 		} catch(InputMismatchException e) {
-			System.out.println("index 須為整數！！");
+			System.out.println("index 須為整數！！ 請重新輸入");
+			main(args);		
 		} catch (ArithmeticException e) {
-			System.out.println("分母不得為零");
+			System.out.println("分母不得為零，請重新輸入");
+			main(args);
+		} catch (Exception e) {   // 父類別要放在最下面！！
+			System.out.println(e.getMessage());
 		}
 		
 	}
