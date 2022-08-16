7ce8f54f204ffd85e2dfdc4e93a02f1e92124c8e
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-08-16 16:10:06 +0800

    practice

--- OCPJP/src/Basic/exception/third/ex2.java
+++ OCPJP/src/Basic/exception/third/ex2.java
@@ -1,5 +1,6 @@
 package Basic.exception.third;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /*
  *  RuntimeException:
@@ -14,16 +15,35 @@ public class ex2 {
 		Scanner sc =new Scanner(System.in);
 		
 		int[] x =new int[10];
-		
-		// x[11]=100; // --> RuntimeException --> java.lang.ArrayIndexOutOfBoundsException     
-		System.out.print("請輸入索引碼：");
-		int n =sc.nextInt(); 
-		System.out.println(x[n]);
-		// 若索引碼超出陣列範圍，則會拋出例外，以下程式碼將不會執行。
-		
-		
-		for(int i=0;i<10;i++) {
-			System.out.println("success");
+		for(int i=0;i<x.length;i++) {
+			x[i]=i;
 		}
+		try {
+			// x[11]=100; // --> RuntimeException --> java.lang.ArrayIndexOutOfBoundsException     
+			System.out.print("請輸入索引碼：");
+			int n =sc.nextInt(); 
+			// 若索引碼超出陣列範圍，則會拋出例外，以下程式碼將不會執行。
+			
+			System.out.print("請輸入分母：");
+			double m =sc.nextDouble();
+			System.out.println("x["+n+"]/"+m+" = "+(x[n]/m));
+			
+			
+			System.out.println("success");
+			
+		} catch (ArrayIndexOutOfBoundsException e) {  // ArrayIndexOutOfBoundsException e =new ArrayIndexOutOfBoundsException();
+			System.out.println("index 須介於 0~9 之間");
+			
+			e.printStackTrace(); // java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 10 at Basic.exception.third.ex2.main(ex2.java:25)
+			System.out.println(e); // java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 10
+			System.out.println(e.toString()); // java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 10
+			System.out.println(e.getMessage()); // Index 10 out of bounds for length 10
+			
+		} catch(InputMismatchException e) {
+			System.out.println("index 須為整數！！");
+		} catch (ArithmeticException e) {
+			System.out.println("分母不得為零");
+		}
+		
 	}
 }
