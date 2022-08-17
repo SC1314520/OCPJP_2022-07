2c027a4e5d4a86229d08cfde59b40c48b1112737
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-08-18 00:32:54 +0800

    practice

--- OCPJP/src/Basic/lambda/function/function/ex.java
+++ OCPJP/src/Basic/lambda/function/function/ex.java
@@ -1,5 +1,18 @@
 package Basic.lambda.function.function;
 
-public class ex {
+import java.util.function.Function;
 
+public class ex {
+	public static int change(String s, Function<String, String> fun1,
+										Function<String, Integer> fun2,
+										Function<Integer, Integer> fun3) {
+		return fun1.andThen(fun2).andThen(fun3).apply(s);
+	}
+	public static void main(String[] args) {
+		String str="貂蟬,20";
+		int i=change(str,s->s.split(",")[1]
+		, s-> Integer.parseInt(s)
+		, num->num+100);
+		System.out.println(i);
+	}
 }
