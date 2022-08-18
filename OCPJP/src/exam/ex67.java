c388af94b5ebce9e56e2095605d2df8c8cf65e4c
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-08-18 19:19:32 +0800

    exam

---
+++ OCPJP/src/exam/ex67.java
@@ -0,0 +1,22 @@
+package exam;
+
+public class ex67 {
+
+}
+/*
+What must be the first characters of a database URL?
+
+A. db,
+B. db:
+C. jdbc,
+D. jdbc:
+E. None of the above
+
+
+
+
+
+ans:D
+
+
+*/
---
+++ OCPJP/src/exam/ex68.java
@@ -0,0 +1,20 @@
+package exam;
+
+public class ex68 {
+
+}
+/*
+Assuming the user credentials are correct, which expression will create a Connection?
+
+A) DriverManager.getConnection("jdbc:derby:com");
+B) DriverManager.getConnection("jdbc.derby.com");
+C) DriverManager.getConnection("http://database.jdbc.com","J_SMITH","dt12%2f3");  // url 不是利用網址   
+D) DriverManager.getConnection();
+E) DriverManager.getConnection("J_SMITH","dt12%2f3");
+
+
+
+
+
+ans:A
+*/
