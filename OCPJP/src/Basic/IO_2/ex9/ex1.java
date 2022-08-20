98350ef3f8876a9d25803a9e961af95538ee9b44
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-08-20 20:46:48 +0800

    practice

--- OCPJP/src/Basic/IO_2/ex9/ex1.java
+++ OCPJP/src/Basic/IO_2/ex9/ex1.java
@@ -1,5 +1,22 @@
 package Basic.IO_2.ex9;
 
-public class ex1 {
+import java.io.File;
+import java.io.FileInputStream;
 
+import java.io.IOException;
+
+public class ex1 {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis =new FileInputStream(new File("/Users/blc0000421gmail.com/Desktop/io/d.txt"));
+//		System.out.println((char)(fis.read()));  //1 讀取一個字
+//		System.out.println((char)(fis.read()));  //0 讀取一個字
+//		System.out.println((char)(fis.read()));  //0 讀取一個字
+		
+		int len=0; // 紀錄讀取數據
+		while((len=fis.read())!=-1) { //  -1 表示空
+			System.out.print((char)(len)); // 100ABCDEBC
+		}
+		
+		fis.close();
+	}
 }
