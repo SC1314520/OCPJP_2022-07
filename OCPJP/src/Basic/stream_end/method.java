bcd83b11d421c12aee8f38045c5e3cea8a73d816
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-08-26 00:35:28 +0800

    end

---
+++ OCPJP/src/Basic/stream_end/method.java
@@ -0,0 +1,33 @@
+package Basic.stream_end;
+
+public class method {
+
+}
+/* 中間方法
+filter()
+map()
+flatmap()
+distinct()
+sorted()
+peek()
+limit()
+skip()
+
+*/
+
+/* 終端方法
+forEach()
+forEachOrdered()
+toArray()
+reduce()
+collect()
+min()
+max()
+count()
+anyMatch()
+allMatch()
+noneMatch()
+findFirst()
+findAny()
+
+*/
